a6356e210980c72635423725fdc0f5dfa5854ee0
zhenyu23 <815512932@qq.com>
2019-11-22 17:01:46 +0800

    Spring cloud本地读取配置和git读取配置

--- eureka-client/src/main/java/com/zy/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/zy/eurekaclient/EurekaClientApplication.java
@@ -1,8 +1,11 @@
 package com.zy.eurekaclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author czy
@@ -10,11 +13,19 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @EnableEurekaClient
 @SpringBootApplication
+//@RestController
 public class EurekaClientApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
 	}
+
+//	@Value("${server.port}")
+//	String foo;
+//	@RequestMapping("/foo")
+//	public String getHi() {
+//		return foo;
+//	}
 
 }
